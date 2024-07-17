22d487189a619e3fecd62eb39b5b7d31d59e9481
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2024-07-17 14:15:12 +0300

    initial packages folder structure

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  singleAttributePerLine: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  proseWrap: "always",
+};
+
+export default config;
